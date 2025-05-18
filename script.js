33eb1d3d31965aa2d2f449b08e1bd84d0c8068a8
Danish Ahmad <108187903+DanishRahi@users.noreply.github.com>
2025-05-18 22:35:15 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const mongoose = require('mongoose')
+
+
+
+
+
+
+// if (bar){
+//     bar.addEventListener('click', () => {
+//         nav.classList.add('active');
+//     })
+// }
+
